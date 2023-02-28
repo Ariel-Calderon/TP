23c4f119f3767ca8a8dc6971e0c643afc88bd906
Ariel Calderón <khaldera@hotmail.com>
2023-02-28 00:23:09 -0300

    Primer commit

---
+++ js/funciones.js
@@ -0,0 +1,54 @@
+
+document.getElementById("acond").onclick = function() {
+    if (document.getElementById("1").style.display == "block"){
+        document.getElementById("1").style.display = "none";
+    } else {
+        document.getElementById("1").style.display = "block";
+    }
+    
+}
+
+document.getElementById("acond").onclick = function() {
+    if (document.getElementById("1").style.display == "block"){
+        document.getElementById("1").style.display = "none";
+    } else {
+        document.getElementById("1").style.display = "block";
+    }
+    
+}
+
+document.getElementById("free").onclick = function() {
+    if (document.getElementById("2").style.display == "block"){
+        document.getElementById("2").style.display = "none";
+    } else {
+        document.getElementById("2").style.display = "block";
+    }
+    
+}
+
+document.getElementById("schipani").onclick = function() {
+    if (document.getElementById("3").style.display == "block"){
+        document.getElementById("3").style.display = "none";
+    } else {
+        document.getElementById("3").style.display = "block";
+    }
+    
+}
+
+document.getElementById("exo").onclick = function() {
+    if (document.getElementById("4").style.display == "block"){
+        document.getElementById("4").style.display = "none";
+    } else {
+        document.getElementById("4").style.display = "block";
+    }
+    
+}
+
+document.getElementById("cof").onclick = function() {
+    if (document.getElementById("5").style.display == "block"){
+        document.getElementById("5").style.display = "none";
+    } else {
+        document.getElementById("5").style.display = "block";
+    }
+    
+}
