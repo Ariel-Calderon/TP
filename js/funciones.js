a72c393f042badd04f5bb4a39a35fa1fe16daa66
Ariel Calderón <khaldera@hotmail.com>
2023-03-01 12:51:12 -0300

    entrega

--- js/funciones.js
+++ js/funciones.js
@@ -2,26 +2,23 @@
 document.getElementById("acond").onclick = function() {
     if (document.getElementById("1").style.display == "block"){
         document.getElementById("1").style.display = "none";
+        document.getElementById("1-1").innerHTML = "(ver más)";
     } else {
         document.getElementById("1").style.display = "block";
+        document.getElementById("1-1").innerHTML = "(ver menos)";
     }
     
 }
 
-document.getElementById("acond").onclick = function() {
-    if (document.getElementById("1").style.display == "block"){
-        document.getElementById("1").style.display = "none";
-    } else {
-        document.getElementById("1").style.display = "block";
-    }
-    
-}
+
 
 document.getElementById("free").onclick = function() {
     if (document.getElementById("2").style.display == "block"){
         document.getElementById("2").style.display = "none";
+        document.getElementById("2-2").innerHTML = "(ver más)";
     } else {
         document.getElementById("2").style.display = "block";
+        document.getElementById("2-2").innerHTML = "(ver menos)";
     }
     
 }
@@ -29,8 +26,10 @@ document.getElementById("free").onclick = function() {
 document.getElementById("schipani").onclick = function() {
     if (document.getElementById("3").style.display == "block"){
         document.getElementById("3").style.display = "none";
+        document.getElementById("3-3").innerHTML = "(ver más)";
     } else {
         document.getElementById("3").style.display = "block";
+        document.getElementById("3-3").innerHTML = "(ver menos)";
     }
     
 }
@@ -38,8 +37,10 @@ document.getElementById("schipani").onclick = function() {
 document.getElementById("exo").onclick = function() {
     if (document.getElementById("4").style.display == "block"){
         document.getElementById("4").style.display = "none";
+        document.getElementById("4-4").innerHTML = "(ver más)";
     } else {
         document.getElementById("4").style.display = "block";
+        document.getElementById("4-4").innerHTML = "(ver menos)";
     }
     
 }
@@ -47,8 +48,10 @@ document.getElementById("exo").onclick = function() {
 document.getElementById("cof").onclick = function() {
     if (document.getElementById("5").style.display == "block"){
         document.getElementById("5").style.display = "none";
+        document.getElementById("5-5").innerHTML = "(ver más)";
     } else {
         document.getElementById("5").style.display = "block";
+        document.getElementById("5-5").innerHTML = "(ver menos)";
     }
     
 }
